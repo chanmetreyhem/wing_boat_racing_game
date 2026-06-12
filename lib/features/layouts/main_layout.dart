de19f11d35dec0cf705715d5a7e14e2235748b3b
Chanmetrey Hem <chanmetrey.hem@wingbank.com.kh>
2026-06-12 08:55:10 +0700

    modifer: change icon of bottom navigation bar

--- lib/features/layouts/main_layout.dart
+++ lib/features/layouts/main_layout.dart
@@ -1,7 +1,7 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter/material.dart';
+import 'package:remixicon/remixicon.dart';
 import 'package:wing_boat_racing_game/features/menu/presentation/view/menu_view.dart';
 import 'package:wing_boat_racing_game/features/profile/presentation/view/profile_view.dart';
 import 'package:wing_boat_racing_game/features/trophy/presentation/view/trophy_view.dart';
@@ -44,16 +44,16 @@ class MainLayout extends HookConsumerWidget {
 
         destinations: [
           NavigationDestination(
-            icon: Icon(CupertinoIcons.house_fill),
+            icon: Icon(RemixIcons.home_2_fill),
             label: 'Home',
           ),
           NavigationDestination(
-            icon: Icon(CupertinoIcons.chart_bar_fill),
+            icon: Icon(RemixIcons.trophy_fill),
             label: 'Trophy',
           ),
 
           NavigationDestination(
-            icon: Icon(CupertinoIcons.person_fill),
+            icon: Icon(RemixIcons.user_fill),
             label: 'Profile',
           ),
         ],
